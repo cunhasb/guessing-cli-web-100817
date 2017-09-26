db8bb9f8ce7302dd6ca4c2d408bd27e603cf99f8
cunhasb CUNHA <cunhasb@aol.com>
2017-09-26 15:04:22 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,9 +5,9 @@ def run_guessing_game
   binding.pry
   until user_input == "exit"
     puts "Guess a number between 1 and 6"
-    user_input = gets.chomp.to_i
+    user_input = gets.chomp
     x = rand(1...7)
-    if (1...7) === user_input
+    if (1...7) === user_input.to_i
        x === user_input ? puts "You guessed the correct number!" : puts "The computer guessed #{x}."
     end
   end
