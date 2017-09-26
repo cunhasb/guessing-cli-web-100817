a8c8ab0d4a7c987fe472e755c29244aef2aebfa6
cunhasb CUNHA <cunhasb@aol.com>
2017-09-26 15:17:36 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ require 'pry'
 def run_guessing_game
   user_input = nil
   until user_input == "exit"
-    puts "Guess a number between 1 and 6"
+    puts "Guess a number between 1 and 6."
     user_input = gets.chomp
     x = rand(1...7)
     if (1...7) === user_input.to_i
