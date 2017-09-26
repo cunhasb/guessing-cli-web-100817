8166c9b1b2575497afcf033a47b66572632f4746
cunhasb CUNHA <cunhasb@aol.com>
2017-09-26 15:03:30 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,12 +1,14 @@
 require 'pry'
 # Code your solution here!
 def run_guessing_game
-  command = nil
+  user_input = nil
   binding.pry
-  until command == "exit"
+  until user_input == "exit"
     puts "Guess a number between 1 and 6"
     user_input = gets.chomp.to_i
+    x = rand(1...7)
     if (1...7) === user_input
+       x === user_input ? puts "You guessed the correct number!" : puts "The computer guessed #{x}."
     end
   end
 end
